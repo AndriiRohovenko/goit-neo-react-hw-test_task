ca911a64e61adee348491e14e094e77fa34b3af5
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-06-08 19:12:54 +0300

    add submit message to form

--- src/components/BookCamperForm/BookCamperForm.jsx
+++ src/components/BookCamperForm/BookCamperForm.jsx
@@ -1,6 +1,7 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styles from './BookCamperForm.module.css';
+import { useState } from 'react';
 
 const BookingForm = () => {
   const initialValues = {
@@ -9,6 +10,8 @@ const BookingForm = () => {
     bookingDate: '',
     comment: '',
   };
+
+  const [showSuccess, setShowSuccess] = useState(false);
 
   const validationSchema = Yup.object({
     name: Yup.string().required('Name is required'),
@@ -22,82 +25,93 @@ const BookingForm = () => {
   const handleSubmit = (values, { resetForm }) => {
     console.log('Form submitted:', values);
     resetForm();
+    setShowSuccess(true);
+    setTimeout(() => {
+      setShowSuccess(false);
+    }, 3000);
   };
 
   return (
-    <div className={styles.formContainer}>
-      <h2 className={styles.formTitle}>Book your camper now!</h2>
-      <p className={styles.formSubtitle}>
-        Stay connected! We are always ready to help you.
-      </p>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {() => (
-          <Form className={styles.bookingForm}>
-            <div className={styles.fieldWrapper}>
-              <label htmlFor="name">Name</label>
-              <Field
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Name*"
-                className={styles.formInput}
-              />
-              <ErrorMessage
-                name="name"
-                component="div"
-                className={styles.error}
-              />
-            </div>
-            <div className={styles.fieldWrapper}>
-              <label htmlFor="email">Email</label>
-              <Field
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email*"
-                className={styles.formInput}
-              />
-              <ErrorMessage
-                name="email"
-                component="div"
-                className={styles.error}
-              />
-            </div>
-            <div className={styles.fieldWrapper}>
-              <label htmlFor="bookingDate">Booking Date</label>
-              <Field
-                type="date"
-                id="bookingDate"
-                name="bookingDate"
-                className={styles.formInput}
-              />
-              <ErrorMessage
-                name="bookingDate"
-                component="div"
-                className={styles.error}
-              />
-            </div>
-            <div className={styles.fieldWrapper}>
-              <label htmlFor="comment">Comment</label>
-              <Field
-                as="textarea"
-                id="comment"
-                name="comment"
-                placeholder="Comment"
-                className={styles.formTextarea}
-              />
-            </div>
-            <button type="submit" className={styles.formButton}>
-              Send
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+    <>
+      {showSuccess ? (
+        <p className={styles.successMessage}>Booking submitted successfully!</p>
+      ) : (
+        <div className={styles.formContainer}>
+          <h2 className={styles.formTitle}>Book your camper now!</h2>
+          <p className={styles.formSubtitle}>
+            Stay connected! We are always ready to help you.
+          </p>
+
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {() => (
+              <Form className={styles.bookingForm}>
+                <div className={styles.fieldWrapper}>
+                  <label htmlFor="name">Name</label>
+                  <Field
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Name*"
+                    className={styles.formInput}
+                  />
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className={styles.error}
+                  />
+                </div>
+                <div className={styles.fieldWrapper}>
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email*"
+                    className={styles.formInput}
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className={styles.error}
+                  />
+                </div>
+                <div className={styles.fieldWrapper}>
+                  <label htmlFor="bookingDate">Booking Date</label>
+                  <Field
+                    type="date"
+                    id="bookingDate"
+                    name="bookingDate"
+                    className={styles.formInput}
+                  />
+                  <ErrorMessage
+                    name="bookingDate"
+                    component="div"
+                    className={styles.error}
+                  />
+                </div>
+                <div className={styles.fieldWrapper}>
+                  <label htmlFor="comment">Comment</label>
+                  <Field
+                    as="textarea"
+                    id="comment"
+                    name="comment"
+                    placeholder="Comment"
+                    className={styles.formTextarea}
+                  />
+                </div>
+                <button type="submit" className={styles.formButton}>
+                  Send
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      )}
+    </>
   );
 };
 
